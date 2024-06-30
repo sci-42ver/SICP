4ada75311e1ca680c809ac1d926f00175cceb82d
sci-42ver <wujsCM@tuta.io>
2024-06-30 11:26:38 +0800

    remove code redundancy

--- exercise_codes/1/1_45_convergence_test/1_45_test_valid_average_damp_level.scm
+++ exercise_codes/1/1_45_convergence_test/1_45_test_valid_average_damp_level.scm
@@ -1,55 +1,12 @@
-;;; from the above wiki
-(define (average-damp f) 
-    (lambda (x) (/ (+ x (f x)) 2)))
-(define (repeated f n) 
-    (define (compose f g) 
-      (lambda (x) (f (g x)))) 
-    (if (= n 1) 
-      f 
-      (compose f (repeated f (- n 1)))))
-
-;;; from book
-(define tolerance 0.00001)
-
-;;; small modified fixed_point
-(define (displayln x)
-  (newline)
-  (display x))
-(define (fixed_point_bound f first-guess bound whether_output_cnt)
-  (define (close-enough? v1 v2)
-    (< (abs (- v1 v2)) tolerance))
-  (define (try guess count)
-    (let ((next (f guess)))
-      ; (displayln guess)
-      (if (or (close-enough? guess next) (= count bound))
-          (begin 
-            ; (displayln "finished count:")
-            ; (displayln count)
-            (displayln next)
-            (if whether_output_cnt
-              count
-              next))
-          (try next (+ count 1)))))
-  ; (displayln "fixed_point_bound:")
-  (try first-guess 1))
-
-(define small_bound 100)
-; (define (init_map x y n) 
-;   (/ x ((repeated (lambda (m) (* y m)) (- n 1)) 1))) 
-(define (root n x average_damp_level bound init whether_output_cnt) 
-  ; (define transformed_f ((repeated average-damp average_damp_level) (lambda (y) (init_map x y n)))) 
-  ;; same as Kaihao's implementation, otherwise the above will output differently although it also converges
-  (define transformed_f ((repeated average-damp average_damp_level) (lambda (y) (/ x (expt y (- n 1)))))) 
-  ; (displayln "average_damp_level:")
-  ; (displayln average_damp_level)
-  (fixed_point_bound transformed_f init bound whether_output_cnt)) 
-(define n2 50)
+(load "minimal_test/1_45_minimal_test.scm")
 ;;; same as the wiki parameter choice.
 (define (find_valid_average_damp_level max_root start_root bound init)
   (define x 2.0)
   (define (helper root_order average_damp_level)
-    (if (< (root root_order x average_damp_level bound init #t) bound)
+    ;; same as Kaihao's implementation, otherwise the above will output differently although it also converges
+    (if (< (root root_order x average_damp_level bound init #t #f #t) bound)
       (begin
+        (displayln "")
         (displayln "needed average_damp_level:")
         average_damp_level)
       (helper root_order (+ average_damp_level 1))))
@@ -62,12 +19,40 @@
       (displayln (helper start_root 1))
       (find_valid_average_damp_level max_root (+ start_root 1) bound init))))
 (find_valid_average_damp_level 20 2 small_bound 1.0)
+;; outputs
+; ...
+; start_root:
+; 6
+;
+; average_damp_level:
+; 1
+; finished count:
+; 100
+; result:
+; .9019353334996821
+;
+; average_damp_level:
+; 2
+; finished count:
+; 15
+; result:
+; 1.1224648393618204
+;
+; needed average_damp_level:
+; 2
+; ...
 
 ;;; check how $sqrt[6]{2}$ converges when "Required Average Damps" is 1.
-(root 6 2.0 1 1000000000 1.0 #t)
+(root 6 2.0 1 1000000000 1.0 #t #f #t)
 ;; outputs with the same result as the following `(nth-root 2.0 6 1)`.
+; average_damp_level:
+; 1
+; finished count:
+; 319396
+; result:
 ; 1.1224584896267153
 ;Value: 319396
+
 
 ;;; from wiki Kaihao's comment
 (define tolerance 0.00001) 
