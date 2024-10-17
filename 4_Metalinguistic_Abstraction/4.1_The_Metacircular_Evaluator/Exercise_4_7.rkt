dff40f42ddd9ef40b59dbef3baec12da0c61001c
sci-42ver <wujsCM@tuta.io>
2024-10-17 17:35:04 +0800

    finish up to section 4.1 exercise 4.8 with checking Z-combinator

--- 4_Metalinguistic_Abstraction/4.1_The_Metacircular_Evaluator/Exercise_4_7.rkt
+++ 4_Metalinguistic_Abstraction/4.1_The_Metacircular_Evaluator/Exercise_4_7.rkt
@@ -12,6 +12,7 @@
       (* x z))))
 
 (define (make-let bindings body)
+  ;; wrong just as LisScheSic's 2nd comment says
   (cons 'let (cons bindings body)))
 
 ; derived expression
@@ -35,3 +36,26 @@
  3)
 
 ; I think explicitly expand let* is same as nested let expressions.
+
+(define test-exp 
+  '(let* ((x 3)
+       (y (+ x 2))
+       (z (+ x y 5)))
+  (* x z)))
+
+; https://stackoverflow.com/q/34852667 https://docs.racket-lang.org/pkg/cmdline.html#%28part._raco-pkg-install%29
+;; https://docs.racket-lang.org/ts-reference/Typed_Units.html omit /unit
+(require typed/racket)
+(define (test)
+  (assert 
+    (equal? '(let ((x 3)) (let ((y (+ x 2))) (let ((z (+ x y 5))) (* x z)))) 
+      (let*->nested-lets test-exp)))
+  (assert 
+    (equal? '(let ((x 1)) (let ((y 2)) (begin x y))) 
+      (let*->nested-lets '(let* ((x 1) (y 2)) x y))))
+  (assert 
+    (equal? '(begin x y) 
+      (let*->nested-lets '(let* () x y))))
+  )
+; (test)
+(let*->nested-lets test-exp)
--- 4_Metalinguistic_Abstraction/4.1_The_Metacircular_Evaluator/Exercise_4_8.rkt
+++ 4_Metalinguistic_Abstraction/4.1_The_Metacircular_Evaluator/Exercise_4_8.rkt
@@ -1,10 +1,10 @@
 #lang racket/base
 
-(let ⟨var⟩ ⟨bindings⟩ ⟨body⟩)
-; named let is equivalent to:
-(let ⟨bindings⟩
-  (define (⟨var⟩ bindings-var-list) body)
-  (⟨var⟩ bindings-var-list))
+; (let ⟨var⟩ ⟨bindings⟩ ⟨body⟩)
+; ; named let is equivalent to:
+; (let ⟨bindings⟩
+;   (define (⟨var⟩ bindings-var-list) body)
+;   (⟨var⟩ bindings-var-list))
 
 (define (make-lambda parameters body)
   (cons 'lambda (cons parameters body)))
@@ -23,10 +23,14 @@
     (cddr exp)))
 
 (define (make-let bindings body)
+  ;; compared with http://community.schemewiki.org/?sicp-ex-4.7 where we use `list` to make `(make-lets (cdr exprs))` right.
+  ;; But here only 1 level, so `cons 'let` is fine. 
   (cons 'let (cons bindings body)))
 
 (define (make-define id expr)
-  (cons 'define (cons id expr)))
+  ;; the difference from wiki meteorgan's is only `(named-let-func-body expr)` where expr is (+ a b) instead of ((+ a b)) here.
+  (cons 'define (cons id expr))
+  )
 
 (define (let->combination exp)
   (let* ([bindings (let-bindings exp)]
@@ -34,6 +38,7 @@
          [exp-list (map cadr bindings)]
          [body (let-body exp)])
     (if (named-let? exp)
+      ;; same as meteorgan's but evaluating args first.
       (make-let bindings
                 (list (make-define (cons (cadr exp) var-list) body)
                       (cons (cadr exp) var-list)))
